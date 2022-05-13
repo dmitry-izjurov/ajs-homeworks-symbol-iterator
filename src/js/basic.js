6d65140317418e1de8884069e9a4815af1d31387
Dmitry <dmitry.izjurov@yandex.ru>
2022-05-13 22:22:23 +0300

    Выполнено задание Символы & итераторы

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,48 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default class Team {
+  constructor() {
+    this.char = [
+      {
+        name: 'Зомби', type: 'Zombie', health: 50, level: 1, attack: 40, defence: 10,
+      },
+      {
+        name: 'Маг', type: 'Magican', health: 50, level: 1, attack: 40, defence: 10,
+      },
+      {
+        name: 'Лучник', type: 'Bowman', health: 50, level: 1, attack: 40, defence: 10,
+      },
+    ];
   }
-  return result;
+}
+
+const command = new Team().char;
+
+const range = {
+  from: 0,
+  to: command.length - 1,
+};
+
+range[Symbol.iterator] = function () {
+  let current = this.from;
+  const last = this.to;
+
+  return {
+    next() {
+      if (current <= last) {
+        const Person = command[current];
+        current += 1;
+        return {
+          done: false,
+          value: Person,
+        };
+      }
+
+      return {
+        done: true,
+      };
+    },
+  };
+};
+
+for (const num of range) {
+  console.log(num);
 }
